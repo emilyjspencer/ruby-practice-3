225db1367d4ee82108403080b77d3aecc9a5330b
lajoiedevivre89 <emilyspencer890@hotmail.co.uk>
2019-11-13 13:52:45 +0000

    The terrible infinite loop

---
+++ infinite_loop_using_while.rb
@@ -0,0 +1,11 @@
+# A method that will print the integer '1' indefinitely
+# IT IS NEVER RECOMMENDED TO USE AN INFINITE LOOP
+# as it can cause your program to crash
+
+def print_1_forever
+  while true do
+    puts 1
+  end
+end 
+
+print_1_forever
