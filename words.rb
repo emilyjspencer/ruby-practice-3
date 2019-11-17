4b4f89117b573be2b13c54526a1849f0365eb44b
lajoiedevivre89 <emilyspencer890@hotmail.co.uk>
2019-11-17 12:51:52 +0000

    Words

--- words.rb
+++ words.rb
@@ -5,4 +5,5 @@ happened with the money (about pro****** form)",  :ch => "You looked as though
 you were crying all the time - your makeup was smudged", :ch1 => "PersonX was
 doing everything perfectly", :ch2 => "PersonX thinks you're having a 
 breakdown", :ch3 => "We didn't think you were doing to make it", :ch4 =>
-"People don't get worse at their jobs", :ch5 => "Stay nice E****", })
+"People don't get worse at their jobs", :ch5 => "Stay nice E****", :ch6 => "Some jobs just don't work out", 
+:ch7 => "I've got to be careful with what I say to you"})
