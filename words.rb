851e9bc398e5130be699952c39ba69ce54744ecf
lajoiedevivre89 <emilyspencer890@hotmail.co.uk>
2019-11-17 12:48:01 +0000

    Words

---
+++ words.rb
@@ -0,0 +1,8 @@
+Array ({:bb => "You made me look stupid in front of her", :bb1 =>
+"I've got ten times the amount of work as you do", :bb2 => 
+"This is what annoys me about you", :bb3 => "I don't know - now that this has
+happened with the money (about pro****** form)",  :ch => "You looked as though
+you were crying all the time - your makeup was smudged", :ch1 => "PersonX was
+doing everything perfectly", :ch2 => "PersonX thinks you're having a 
+breakdown", :ch3 => "We didn't think you were doing to make it", :ch4 =>
+"People don't get worse at their jobs", :ch5 => "Stay nice E****", })
