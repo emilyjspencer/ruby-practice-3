61499460afc36cb0b8b36fd273c63307f5a52530
lajoiedevivre89 <emilyspencer890@hotmail.co.uk>
2019-11-21 00:49:11 +0000

    Adding a few of my books to a hash

---
+++ adding_to_hashes.rb
@@ -0,0 +1,20 @@
+books = {}
+
+books[:frankenstein] = "Mary Shelley"
+books[:the_path] = "Professor Michael Puett"
+books[:proust_and_the_squid] = "Maryanne Wolf" 
+books[:spark!] = "Dr John Ratey"
+books[:the_secret] = "Rhonda Byrne"
+books[:option_b] = "Sheryl Sandberg"
+books[:through_the_language_glass] = "Guy Deutscher"
+books[:the_romans_for_dummies] = "Guy de la Bedoyérè"
+books[:on_beauty] = "Zadie Smith"
+
+puts books
+
+# => {:frankenstein=>"Mary Shelley", :the_path=>"Professor Michael Puett", 
+# :proust_and_the_squid=>"Maryanne Wolf", :spark!=>"Dr John Ratey", 
+# :the_secret=>"Rhonda Byrne", :option_b=>"Sheryl Sandberg", 
+# :through_the_language_glass=>"Guy Deutscher", 
+# :the_romans_for_dummies=>"Guy de la Bedoyérè", :on_beauty=>"Zadie Smith"}
+
